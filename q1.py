6d1426a8afd78a273e7f9e05453bb437ab33c4c8
yean.ha <yean.ha@servicenow.com>
2018-10-17 21:34:34 +0100

    brute q1 done

--- q1.py
+++ q1.py
@@ -5,5 +5,12 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  answer = 0
-  return answer
+  maxeval = 0
+  n = len(portfolios)
+  for i in range(n):
+    for j in range(i+1,n):
+      curvalue = portfolios[i] ^ portfolios[j]
+      if curvalue > maxeval:
+        maxeval = curvalue
+    
+  return maxeval
