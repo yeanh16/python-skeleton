68308a3a924a7170aad54f74aced9524479c9b00
yean.ha <yean.ha@servicenow.com>
2018-10-18 20:06:27 +0100

    q5

--- q5.py
+++ q5.py
@@ -1,8 +1,17 @@
 # ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
 
 import numpy as np
+import sys
 
 def question05(allowedAllocations, totalValue):
-  # modify and then return the variable below
-  answer = -1
-  return answer
+  amount = [0 for _ in range(totalValue+1)]
+  for amt in range(1, totalValue+1):
+    amount[amt] = sys.maxint
+    temp = sys.maxint
+    for c in range(0, len(allowedAllocations)):
+      if allowedAllocations[c] <= amount:
+        temp_amt = amount[amt-allowedAllocations[c]] + 1
+        if temp_amt < temp:
+          temp = temp_amt
+          amount[amt] = temp
+  return amount[totalValue]
