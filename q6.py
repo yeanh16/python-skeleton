e4c3136e0602f5dcb5bab8d04fab9f39c798446a
yean.ha <yean.ha@servicenow.com>
2018-10-20 00:14:17 +0100

    hi

--- q6.py
+++ q6.py
@@ -2,7 +2,14 @@
 
 import numpy as np
 
+## input: [3,1,[[0,7,4],[7,0,2],[4,2,0]] output: 6
+## number of servers and target server
+
+
+
 def question06(numServers, targetServer, times):
   # modify and then return the variable below
+  Q = [i for i in range(numServers)]
+  
   answer = -1
   return answer
